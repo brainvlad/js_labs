6dc18bc89c1ccd24e24f09dd871f08895a788c68
brainvlad <wladiik@gmail.com>
2021-09-26 18:59:02 +0300

    add all tasks

--- 4_lab/script.js
+++ 4_lab/script.js
@@ -60,7 +60,7 @@ function task2() {
 function task3() {
     console.log('task 3:')
 
-    const weekDays = {
+    const WeekDays = {
         1 : 'понедельник',
         2 : 'вторник',
         3 : 'среда',
@@ -71,7 +71,7 @@ function task3() {
     };
 
     let presentDay = new Date(Date.now()).getDay();
-    const daysOfWeek = Object.keys(weekDays);
+    const daysOfWeek = Object.keys(WeekDays);
     let countOddDays = 0;
 
     if (presentDay === 0) {
@@ -80,11 +80,81 @@ function task3() {
 
     daysOfWeek.forEach(day => {
         if (day % 2 !== 0) {
-            console.log(weekDays[day]);
+            console.log(WeekDays[day]);
             countOddDays++;
         }
     });
 
     console.log(`count odd days: ${countOddDays}`);
-    console.log(`present Day: ${weekDays[presentDay]}`);
+    console.log(`present Day: ${WeekDays[presentDay]}`);
+}
+
+//  task 4
+//  На сайте есть слайдер.
+//  Его можно настраивать:
+//  пользователь может задать заголовок слайдера (его имя);
+//  можно скрывать/показывать кнопки управления Вперед/Назад и их размер (ширину);
+//  задавать количество слайдов в ряду;
+//  установить размер (высоту, ширину) слайдера с учетом кнопок.
+//  Создайте объект slider с соответствующими свойствами.
+//  Рассчитайте ширину слайдов, если оступы между ними 5px.
+//  Допустим, возникла необходимость создать на странице еще один такой же слайдер, но с другим заголовком.
+//  Как это сделать?
+//  Задайте новые размеры слайдеру и пересчитайте отступы.
+
+function task4() {
+    console.log('task 4:');
+
+    const Slider = {
+        title: 'Slider-1',
+        hideBtnControls: true,
+        sizeBtnControls: 50,
+        countSlidesInRow: 5,
+        marginSlides: 5,
+        height: 300,
+        width: 1000 + (this.sizeBtnControls * 2),
+        widthFullSlider: (this.width - (this.sizeBtnControls * 2) -
+                         (this.countSlidesInRow * (this.marginSlides * 2))) /
+                         (this.countSlidesInRow)
+    }
+
+    const Slider2 = Object.assign({}, Slider);
+    Slider2.title = 'Slider-2';
+
+    console.log(Slider);
+    console.log(Slider2);
+}
+
+// task 5
+// На сайте все элементы оформлены одинаково.
+// У всех кнопок одинаковые ширина, высота, цвет фона и текста.
+// Все ссылки имеют одинаковые размер шрифта, гарнитуру и цвет текста.
+// Для привлечения внимания элементы выделяют желтым цветом (цвет фона).
+// Создайте объекты для кнопок, для ссылок и акцентных элементов (3 объекта!!).
+// Используя эти объекты, создайте акцентные кнопки и ссылки.
+
+function task5() {
+    const Button = {
+        width: 100,
+        height: 50,
+        textColor: '#fefefe',
+        bgColor: '#333333'
+    };
+
+    const Link = {
+        fontSize: 14,
+        fontFamily: 'Times New Roman',
+        textColor: '#5388ce'
+    };
+
+    const Accent = {
+        bgColor: '#fbeeb4'
+    };
+
+    const btn1 = Object.assign(Button, Accent);
+    const link1 = Object.assign(Link, Accent);
+
+    console.log(btn1);
+    console.log(link1);
+
 }
