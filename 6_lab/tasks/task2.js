15d233f63ea58b8d7a8786d7c1c8de330e0bbf28
brainvlad <wladiik@gmail.com>
2021-09-27 12:21:14 +0300

    add task 1, 2, 3

---
+++ 6_lab/tasks/task2.js
@@ -0,0 +1,37 @@
+// task 2
+// Что будет выведено в следующих примерах?
+// Почему?
+
+function task2() {
+    console.group('task2');
+
+    // window.a;
+    // console.log(a); // a is not defined
+    //
+    // console.log(a); // a is not defined
+    // window.a = 2;
+    //
+    // console.log(a); // a is not defined
+    // window.a = 2;
+    // let a;
+    //
+    // console.log(a); // undefined
+    // window.a = 2;
+    // var a;
+    //
+    // console.log(a);
+    // let a = 2; //a is not defined (let vs var)
+    //
+    // let a = 2;
+    // window.a = 3;
+    // console.log(a); // 2: variable a defined by let
+    //
+    // var a = 2;
+    // window.a = 3;
+    // console.log(a); // 3: variable a defined by var
+
+    console.groupEnd();
+}
+
+task2();
+
